c2fc17028abbbfc6bdca5ade0b4c83a23fa8ecb6
James Kenney <jkenney2@charter.net>
2020-10-31 16:21:17 -0400

    Opmodes updated

---
+++ TeamCode/src/org/firstinspires/ftc/teamcode/mecbot/opmodes/GoalBotTeleOp.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.mecbot.opmodes;
+
+import org.firstinspires.ftc.teamcode.mecbot.MecBotTeleOp;
+
+public class GoalBotTeleOp extends MecBotTeleOp {
+
+    public void runOpMode(){
+
+    }
+
+}
